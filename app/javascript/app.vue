4e4c1cf061572a78d4135639beaf527bdd1cb3f7
Miguel Torres <migueltg93@gmail.com>
2018-05-11 22:05:52 -0500

    CardMoved method

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -4,9 +4,11 @@
       <h5>{{ list.name }}</h5>
       <hr>
 
-      <div v-for="(card, index) in list.cards" class="card card-body mb-3">
-        {{ card.name }}
-      </div>
+      <draggable v-model="list.cards" :options="{group: 'cards'}" class="dragArea" @change="cardMoved">
+        <div v-for="(card, index) in list.cards" class="card card-body mb-3">
+          {{ card.name }}
+        </div>
+      </draggable>
 
       <div class="card card-body">
         <textarea class="form-control" v-model="messages[list.id]"></textarea>
@@ -30,9 +32,30 @@ export default {
   },
 
   methods: {
+    cardMoved: function(event) {
+      if (event.added == undefined) { return }
+
+      const list_index = this.lists.findIndex((list) =>{
+          return list.cards.find((card) => {
+            return card.id === event.added.element.id
+          })
+      })
+
+      var data = new FormData
+      data.append("card[list_id]", this.lists[list_index].id)
+      data.append("card[position]", event.added.newIndex + 1)
+
+      Rails.ajax({
+        url: `/cards/${event.added.element.id}/move`,
+        type: "PATCH",
+        data: data,
+        dataType: "json"
+      })
+    },
+
     listMoved: function(event)  {
       var data = new FormData
-      data.append("list[position]", event.newIndex + 1)
+      data.append("card[position]", event.added.newIndex + 1)
 
       Rails.ajax({
         url: `/lists/${this.lists[event.newIndex].id}/move`,
